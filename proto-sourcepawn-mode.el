72c5bd0743a5499ff5f7a546278d9ca8282bf688
Aaron Griffith <aargri@gmail.com>
2010-08-05 08:03:05 -0400

    removed fixme for website, modded address a bit

--- proto-sourcepawn-mode.el
+++ proto-sourcepawn-mode.el
@@ -6,8 +6,7 @@
 ;; found at <http://www.sourcemod.net/>,
 ;;
 ;; More (and nicer) documentation for sourcepawn-mode may be found at
-;; <http://gamma-level.com/teamfortress/sourcepawn-mode>.
-;; [FIXME: make website]
+;; <http://gamma-level.com/teamfortress2/sourcepawn-mode>.
 ;;
 ;; Suggestions, improvements, and bug reports are welcome. Please
 ;; contact me at the email address above!
